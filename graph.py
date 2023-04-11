bf832a8ef1636ed69233fd223140304dd74d8402
Виктор Шляпин <shliapin.vm@phystech.edu>
2023-04-11 10:02:17 +0300

    first python commit - graph

---
+++ graph.py
@@ -0,0 +1,36 @@
+a = list(map(int, input().split()))
+b = []
+stack = [0]
+visited = []
+
+for i in range(a[1]):
+    b.append(list(map(int, input().split())))
+
+def func_1(stack, visited, b, d, n):
+    if n == 0:
+        return 0
+    stack_1 = stack.copy()
+    stack = []
+    b_1 = []
+    for i in range(len(visited)):
+        for j in range(len(b)):
+            if b[j][0] != visited[i] and b[j][1] != visited[i]:
+                b_1.append(b[j])
+    if visited == []:
+        b_1 = b
+    for i in range(len(stack_1)):
+        for j in range(len(b_1)):
+            if b_1[j][0] == stack_1[i]:
+                stack.append(b_1[j][1])
+            if b_1[j][1] == stack_1[i]:
+                stack.append(b_1[j][0])
+    visited = visited + stack_1
+    d += 1
+    for i in range(len(stack)):
+        if n == stack[i]:
+            return d
+    return func_1(stack, visited, b_1, d, n)
+
+
+for i in range(a[0]):
+    print(func_1(stack, visited, b, 0, i))
